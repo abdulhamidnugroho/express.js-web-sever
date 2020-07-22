df6e2c4b583f65d57bef2af65a1d7205f7b04baf
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-07-22 11:41:14 +0700

    setting up html and json response

--- src/app.js
+++ src/app.js
@@ -3,19 +3,31 @@ const express = require('express')
 const app = express()
 
 app.get('', (req, res) => {
-    res.send('Helllo EXPREESS')
+    res.send('<h1>Helllo EXPREESS</h1>')
 })
 
 app.get('/help', (req, res) => {
-    res.send('Help Page')
+    res.send([{
+        name: 'John',
+        age: 20
+    }, {
+        name: 'Web',
+        age: 20
+    }])
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    res.send('<h1>About Page</h1>')
 })
 
 app.get('/weather', (req, res) => {
-    res.send('Weather Page')
+    res.send([{
+        location: 'London',
+        forecast: 'Rainy, 50 degree'
+    }, {
+        location: 'Greenwich',
+        forecast: 'Snowy, 6 degree'
+    }])
 })
 
 app.listen(3000, () => {
