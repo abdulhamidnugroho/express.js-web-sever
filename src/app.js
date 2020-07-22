e8eb51170a6094d537baf3792a050e3cd768a79c
abdul_hamid <abdulhamidnugroho@gmail.com>
2020-07-22 14:31:34 +0700

    dynamic templating with engine (handlebars (hbs))

--- src/app.js
+++ src/app.js
@@ -1,24 +1,41 @@
 const express = require('express')
 const path = require('path')
-const app = express()
 
+const app = express()
 const publicDir = path.join(__dirname, '../public')
 
+app.set('view engine', 'hbs')
 app.use(express.static(publicDir))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'John'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'John Deacon'
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        message: 'Help message',
+        phone: '09372'
+    })
+})
 
 app.get('/weather', (req, res) => {
     res.send([{
         location: 'London',
         forecast: 'Rainy, 50 degree'
-    }, {
-        location: 'Greenwich',
-        forecast: 'Snowy, 6 degree'
     }])
 })
 
 app.listen(3000, () => {
     console.log('Server is Up on port 3000')
 })
-
-
-
